f68c5643c5ec465a9337d8f5b0e32f14ee3c183c
Ann212 <scorpionbleu5639@gmail.com>
2023-01-19 12:12:31 -0500

    making PORT changes to app.js

--- app.js
+++ app.js
@@ -28,8 +28,9 @@ app.set('view engine', '.hbs')
 
 //Routes
 app.use('/', require('./routes/index'))
+app.use('/dashboard', require('./routes/index'))
 
-const PORT= process.env.PORT || 8500
+const PORT= process.env.PORT || 8000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
 
