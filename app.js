78c24f0a71a42d2eb34ccb34e3f32d4bf574394d
Ann212 <scorpionbleu5639@gmail.com>
2023-01-25 12:30:25 -0500

    making changes to app.js

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const express = require ('express')
 const dotenv = require ('dotenv')
 const morgan = require('morgan')
 const exphbs = require ('express-handlebars')
+const passport = require ('passport')
+const session = require('express-session')
 const connectDB = require ('./config/db') //import function
 
 
 //load config 
 dotenv.config({ path: './config/config.env'})
+
+//Passport config 
+require('./config/passport')(passport)
+
 
 connectDB()//calling function 
 
@@ -21,11 +27,24 @@ if (process.env.NODE_ENV === 'development' ){
 
 //Handlebars 
 app.engine('.hbs', exphbs.engine({
-    defaultLayout:'main', //apply layout all of body elements
+defaultLayout:'main', //apply layout all of body elements
     extname:'.hbs'
     })
 )
 app.set('view engine', '.hbs')
+
+//session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false, 
+    saveUninitialized:true,
+    cookie:{secure:true} 
+})) 
+
+
+//passport middleware 
+app.use(passport.initialize())
+app.use(passport.session())
 
 //Static Folder 
 app.use(express.static(path.join(__dirname,'public')))
