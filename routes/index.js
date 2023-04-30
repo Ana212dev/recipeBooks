6c4999599e8d6c63054278fcc7cfed2fdb1bb23e
Ann212 <scorpionbleu5639@gmail.com>
2023-04-30 12:21:28 -0400

    modified routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const { ensureAuth, ensureGuest } = require('../middleware/auth')
+const { ensureAuth, ensureGuest} = require('../middleware/auth')
 
-
-//@description Login/Landing page
-//@route GET/
-router.get('/', ensureGuest, (req, res) => {
-    res.render('login',
-    {layout: 'login' }
-    )
+//@desc Login/landing page
+//@route GET /
+router.get('/', ensureGuest, (req,res) => {
+    res.render('login', {
+        layout: 'login',
+    })
 })
 
-
-//@description Dashboard
-//@route GET/Dashboard
-router.get('/dashboard', ensureAuth, (req, res) => {
-    console.log (req.user)
+//@desc Dashboard
+//@route GET /dashboard
+router.get('/dashboard', ensureAuth, (req,res) => {
+    console.log(req.user)
     res.render('dashboard')
 })
+
 module.exports = router
