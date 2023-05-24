5a454a86da6b35075b4f348d6c07a9110e4cde9d
Ann212 <scorpionbleu5639@gmail.com>
2023-05-24 12:15:37 -0400

    making changes to routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,31 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest} = require('../middleware/auth')
 
+const Recipe = require ('../models/recipe')
+
 //@desc Login/landing page
 //@route GET /
 router.get('/', ensureGuest, (req,res) => {
     res.render('login', {
         layout: 'login',
+    
     })
 })
 
 //@desc Dashboard
 //@route GET /dashboard
-router.get('/dashboard', ensureAuth, (req,res) => {
-    console.log(req.user)
-    res.render('dashboard', {
-        name: req.user.firstName,
-    })
-})
+router.get('/dashboard', ensureAuth, async (req, res) => {
+    try {
+        const recipe = await Recipe.find({user: req.user.id}).lean()
+        res.render('dashboard', {
+            name: req.user.firstName,
+            recipe
+        })
+    
+        }catch (err) {
+            console.error(err)
 
+    }
+
+})
 module.exports = router
